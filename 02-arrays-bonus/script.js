61f9211f477b20c70d3cfe91edfcae696b2ce51e
nicolo_canevari <nico.canevari@outlook.it>
2024-12-19 15:15:08 +0100

    Prima parte esercizio bonus + aggiunta coomenti esercizio 1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,13 +10,36 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+// inverto l'ordine dell'array salvandolo nella variabile reversedTeachers
+const reversedTeachers = teachers.reverse();
+
+console.log(reversedTeachers);
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+//  usando .filter e assegnandogli un valore filtro gli insegnanti con nomi >= 5 lettere
+const longNames = teachers.filter(teacher => teacher.length >= 5);
+
+console.log(longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+// Trova l'indice di 'Ed'
+const indexOfEd = teachers.indexOf('Ed');  
+
+console.log(indexOfEd);
+
+// Se 'Ed' è trovato lo rimuovo dall'array
+if (indexOfEd !== -1) {  
+  teachers.splice(indexOfEd, 1);
+}
+
+console.log(teachers);
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
--- script.js
+++ script.js
@@ -10,18 +10,24 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+
+// L'indice 3 corrisponde a Phil
 const fourthTeacher = teachers[3];
 
 console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+
+// Carlo diventa Patrick
 teachers[4] = 'Patrick'; 
 
 console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+
+// con .pop rimuovo Luca e lo assegno a const lastTeacher
 const lastTeacher = teachers.pop();
 
 console.log(lastTeacher);
@@ -30,6 +36,8 @@ console.log(teachers);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+
+// con .shift rimuovo Nathan e lo assegno a const firstTeacher
 const firstTeacher = teachers.shift();
 
 console.log(firstTeacher);
@@ -39,6 +47,7 @@ console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
+// con .push aggiungo alla fine dell'array Vanessa
 teachers.push('Vanessa');
 
 console.log(teachers);
@@ -46,6 +55,7 @@ console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
+// con .unshift aggiungo Sarah all'inizio dell'array
 teachers.unshift('Sarah');
 
 console.log(teachers);
@@ -53,6 +63,8 @@ console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
+
+// con .indexOf trovo l'indice di Lewis
 const lewisIndex = teachers.indexOf('Lewis');
 
 console.log(lewisIndex);
@@ -60,6 +72,8 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
+
+//  verifico se l'array è vuoto controllando la lunghezza del contenuto.
 const isTeachersEmpty = teachers.length === 0;  
 
 console.log(isTeachersEmpty);
